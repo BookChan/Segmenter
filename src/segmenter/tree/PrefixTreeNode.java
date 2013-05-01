1cf79ca6d6111d2536d6e4c6f8974a80f7dea3c1
gagazhn <gaga.zhn@gmail.com>
2013-05-01 10:38:23 +0800

    just update

--- src/segmenter/tree/PrefixTreeNode.java
+++ src/segmenter/tree/PrefixTreeNode.java
@@ -1,6 +1,7 @@
 package segmenter.tree;
 
 import java.util.HashMap;
+import java.util.HashSet;
 
 import com.sleepycat.persist.model.Entity;
 import com.sleepycat.persist.model.PrimaryKey;
@@ -11,9 +12,10 @@ public class PrefixTreeNode {
 	public static long sss = 0;
 	
 	@PrimaryKey
-	public String word;
-	public PrefixTreeNode parent;
-	public HashMap<String, PrefixTreeNode> children;
+	private String word;
+	
+	public String parent;
+	public HashSet<String> children;
 	
 	// compile完成后不变，作为FM,HS,HM的依据
 	public int count;
@@ -26,7 +28,7 @@ public class PrefixTreeNode {
 	// depth of the tree node
 	public int depth;
 	
-	public PrefixTreeNode(String word, PrefixTreeNode parent, int depth) {
+	public PrefixTreeNode(String word, String parent, int depth) {
 		this.word = word;
 		this.count = 0;
 		this.sum = 0;
@@ -41,9 +43,10 @@ public class PrefixTreeNode {
 	
 	public PrefixTreeNode addWord(String word) {
 		if (this.children == null) {
-			this.children = new HashMap<String, PrefixTreeNode>();
+			this.children = new HashSet<String>();
 		}
 		
+		this.children.add(word);
 		PrefixTreeNode node = this.children.get(word);
 		if (node == null) {
 			node = new PrefixTreeNode(word, this, depth + 1);
